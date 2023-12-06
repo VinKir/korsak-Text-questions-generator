a7ac6acd46393b47f5e61b4c03f887cd25a64c6a
VinKir <VinitKirill@gmail.com>
2023-12-06 14:54:50 +0500

    feat: get text from file by button, get questions by button, get answers by button

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+import requests
+from bs4 import BeautifulSoup
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
-import pinecone
 import os
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
@@ -21,55 +22,84 @@ root.grid_columnconfigure(index=0, weight=1)
 root.grid_columnconfigure(index=1, weight=1)
 root.grid_columnconfigure(index=2, weight=1)
 root.grid_columnconfigure(index=3, weight=1)
+root.grid_columnconfigure(index=4, weight=1)
+root.grid_columnconfigure(index=5, weight=1)
 
+text_editor_label = Label(text="Текст")
+text_editor_label.grid(column=0, columnspan=2, row=0)
 text_editor = Text()
-text_editor.grid(column=0, columnspan=2, row=0)
+text_editor.grid(column=0, columnspan=2, row=1)
 
+questions_text_editor_label = Label(text="Вопросы")
+questions_text_editor_label.grid(column=2, columnspan=2, row=0)
 questions_text_editor = Text()
-questions_text_editor.grid(column=2, columnspan=2, row=0)
+questions_text_editor.grid(column=2, columnspan=2, row=1)
+
+answers_text_editor_label = Label(text="Ответы")
+answers_text_editor_label.grid(column=4, columnspan=2, row=0)
+answers_text_editor = Text()
+answers_text_editor.grid(column=4, columnspan=2, row=1)
 
 
-# открываем файл в текстовое поле
 def open_file():
     filepath = filedialog.askopenfilename()
     if filepath != "":
-        with open(filepath, "r") as file:
+        with open(filepath, "r", encoding = "utf-8") as file:
             text = file.read()
             text_editor.delete("1.0", END)
             text_editor.insert("1.0", text)
+            print("Проверка1")
             return text
+
 
 def chat_questions(user_input, chat):
     messages = [SystemMessage(
-        content='Ты преподаватель, твоя цель сгенерировать вопросы по тексту, который ведёт пользователь, так чтобы проверить его знания по этому тексту, например: текст:{Меня зовут Алёна , мне 32 года}, твой вопрос:{Как тебя зовут?}')
+        content='Ты преподаватель, твоя цель сгенерировать 5 - 6 вопросов по тексту, который ведёт пользователь, так чтобы проверить его знания по этому тексту, например: текст:{Меня зовут Алёна , мне 32 года}, твой вопрос:{Как тебя зовут?}')
         , HumanMessage(content=user_input)]
+    res = chat(messages)
+    messages.append(res)
+    print("Проверка2")
+
+    return res.content
+
+
+def chat_answer(questions, chat):
+    messages = [SystemMessage(
+        content='Тебе надо сгенерировать ответы на вопросы по тексту, который тебе пришлёт пользователь'),
+        HumanMessage(content=questions)]
     res = chat(messages)
     messages.append(res)
     return res.content
 
-
-# сохраняем текст из текстового поля в файл
-def generate_questions():
-    questions = start_place()
-    questions_text_editor.delete("1.0", END)
-    questions_text_editor.insert("1.0", questions)
-
-
-def start_place():
-    # Авторизация в сервисе GigaChat
+class ChatBot:
     chat = GigaChat(
         credentials="MDQ0MzhkMTYtZTQ0NS00M2M0LWI5OGItNmRmZDdkYTNkZmFmOjA0MDliNzIzLTU0YjYtNDY3OC1iMjVjLTY4MjczYjExOWU3Yg==",
         verify_ssl_certs=False)
-    user_input = open_file()
-    #user_input = input("User: ")
-    questions = chat_questions(user_input, chat=chat)
-    return f"Вопросы: \n {questions}"
+    
+
+def generate_questions():
+    user_input = text_editor.get("1.0", "end")
+    questions = chat_questions(user_input, chat=ChatBot.chat)
+    #questions = start_place(user_input, chat_questions)
+    questions_text_editor.delete("1.0", END)
+    questions_text_editor.insert("1.0", questions)
+    print(questions)
+
+def generate_answer():
+    text = text_editor.get("1.0", "end") + questions_text_editor.get("1.0", "end")
+    answers = chat_answer(text, chat=ChatBot.chat)
+    answers_text_editor.delete("1.0", END)
+    answers_text_editor.insert("1.0", answers)
+    print(answers)
 
 
 open_button = ttk.Button(text="Открыть файл", command=open_file)
-open_button.grid(column=0, row=1, sticky=NSEW, padx=10)
+open_button.grid(column=0, row=2, sticky=NSEW, padx=10)
 
-save_button = ttk.Button(text="Сгенерировать вопросы", command=generate_questions)
-save_button.grid(column=2, row=1, sticky=NSEW, padx=10)
+generate_questions_button = ttk.Button(text="Сгенерировать вопросы", command=generate_questions)
+generate_questions_button.grid(column=2, row=2, sticky=NSEW, padx=10)
+
+get_answers_button = ttk.Button(text="Ответить на вопросы", command=generate_answer)
+get_answers_button.grid(column=4, row=2, sticky=NSEW, padx=10)
 
 root.mainloop()
